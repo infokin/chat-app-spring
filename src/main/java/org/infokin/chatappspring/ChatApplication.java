02b85ed9601d1f8d3345cfaddd2068e3b7f526b6
Johannes Hillert <johannes.hillert@infokin.org>
2022-09-05 19:10:52 +0200

    CHAT-7 Initial commit

---
+++ src/main/java/org/infokin/chatappspring/ChatApplication.java
@@ -0,0 +1,13 @@
+package org.infokin.chatappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}
