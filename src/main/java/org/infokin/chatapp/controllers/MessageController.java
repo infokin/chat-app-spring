ca82de99a84301aff0fe44582b8ef976395b5a97
Johannes Hillert <johannes.hillert@infokin.org>
2022-09-20 20:48:54 +0200

    CHAT-23 Add documentation for REST API

--- src/main/java/org/infokin/chatapp/controllers/MessageController.java
+++ src/main/java/org/infokin/chatapp/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package org.infokin.chatapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.infokin.chatapp.models.Message;
 import org.infokin.chatapp.services.MessageService;
 import org.springframework.http.MediaType;
@@ -18,6 +19,10 @@ public class MessageController {
     this.messageService = messageService;
   }
 
+  @Operation(
+    summary = "Send a message",
+    description = "Send a chat message to the server."
+  )
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   public void receiveMessage(@RequestBody Message message) {
     this.messageService.addMessage(message);
