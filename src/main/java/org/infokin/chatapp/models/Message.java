859bf8f2e5a64176dff525d77a0178a6dfc52db9
Johannes Hillert <johannes.hillert@infokin.org>
2022-11-19 19:53:53 +0100

    CHAT-34 Fix for failing POST request

--- src/main/java/org/infokin/chatapp/models/Message.java
+++ src/main/java/org/infokin/chatapp/models/Message.java
@@ -2,8 +2,10 @@ package org.infokin.chatapp.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Message {
 
