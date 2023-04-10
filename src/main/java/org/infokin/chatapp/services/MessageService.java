254bc8862f32306eaa4efd1668c1180a1242ad2c
Johannes Hillert <johannes.hillert@infokin.org>
2023-04-10 16:12:10 +0200

    CHAT-47 Remove logging from service

--- src/main/java/org/infokin/chatapp/services/MessageService.java
+++ src/main/java/org/infokin/chatapp/services/MessageService.java
@@ -1,20 +1,17 @@
 package org.infokin.chatapp.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.infokin.chatapp.models.Message;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Slf4j
 @Service
 public class MessageService {
 
   private final List<Message> messages = new ArrayList<>();
 
   public void addMessage(Message message) {
-    log.debug("Adding message: " + message);
     messages.add(message);
   }
 
